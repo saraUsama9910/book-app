293e4a3a1c32e60cae862e8a602cb2aa4e99f076
saraUsama9910 <sarausama80@gmail.com>
2024-03-22 03:58:00 +0200

    add splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/Features/splash/peresntation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +12,7 @@ class BookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
-      
+      home: SplashView(),
     );
   }
 }
