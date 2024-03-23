f94f7367eccf5e117ed795e4e0edf65126ebb67c
saraUsama9910 <sarausama80@gmail.com>
2024-03-23 21:40:23 +0200

    add font style

--- lib/Features/home/peresntation/views/widgets/home_view_body.dart
+++ lib/Features/home/peresntation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:book_app/Features/home/peresntation/views/widgets/custom_app_bar.dart';
 import 'package:book_app/Features/home/peresntation/views/widgets/featured_books_list_view.dart';
+import 'package:book_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBoy extends StatelessWidget {
@@ -7,8 +8,26 @@ class HomeViewBoy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBar(), FeaturedBooksListView()],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 10,
+            width: 15,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 12, bottom: 30),
+            child: Text(
+              'Best Seller',
+              style: Styles.textStyle20,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromARGB(255, 8, 10, 43);
 const kTransitionDuration = Duration(milliseconds: 250);
+const kGtSectraFine = 'GT Sectra Fine';
