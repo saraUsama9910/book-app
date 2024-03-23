d38994164ccd469ff7c2ba372daa20c318f191b6
saraUsama9910 <sarausama80@gmail.com>
2024-03-23 20:23:16 +0200

    add boxfit to image

--- lib/Features/home/peresntation/views/widgets/home_view_body.dart
+++ lib/Features/home/peresntation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/Features/home/peresntation/views/widgets/custom_app_bar.dart';
+import 'package:book_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBoy extends StatelessWidget {
@@ -7,7 +8,25 @@ class HomeViewBoy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [CustomAppBar(), CustomListViewItem()],
+    );
+  }
+}
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 140,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage(AssetsData.testImage),
+        ),
+      ),
     );
   }
 }
