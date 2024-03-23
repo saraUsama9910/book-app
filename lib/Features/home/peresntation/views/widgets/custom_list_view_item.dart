407d13e2937d37cf0a778514ea59e4275d948dcc
saraUsama9910 <sarausama80@gmail.com>
2024-03-23 20:42:10 +0200

    item done

--- lib/Features/home/peresntation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/peresntation/views/widgets/custom_list_view_item.dart
@@ -7,14 +7,15 @@ class CustomListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 100,
+      height: MediaQuery.of(context).size.height * .3,
       child: AspectRatio(
-        aspectRatio: 2 / 4,
+        aspectRatio: 2.7 / 4,
         child: Container(
           // height: MediaQuery.of(context).size.height * .3,
           // width: 120,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: const DecorationImage(
               fit: BoxFit.fill,
               image: AssetImage(AssetsData.testImage),
             ),
