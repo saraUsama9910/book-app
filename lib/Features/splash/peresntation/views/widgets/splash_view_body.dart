11e1e1b3edcc1d7876bbf907865a7a94a04ba73e
saraUsama9910 <sarausama80@gmail.com>
2024-03-23 04:35:08 +0200

    change form ststeless to stateful in splash view body

--- lib/Features/splash/peresntation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/peresntation/views/widgets/splash_view_body.dart
@@ -1,9 +1,15 @@
 import 'package:book_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  late AnimationController animationController;
   @override
   Widget build(BuildContext context) {
     return Column(
