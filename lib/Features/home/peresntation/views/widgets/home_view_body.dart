161b9de190a740b5ce0f627ffa245ca361794d5f
saraUsama9910 <sarausama80@gmail.com>
2024-03-29 11:43:33 +0200

    remove padding from featured bookks llist view

--- lib/Features/home/peresntation/views/widgets/home_view_body.dart
+++ lib/Features/home/peresntation/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:book_app/Features/home/peresntation/views/widgets/best_seller_list_view_item.dart';
+import 'package:book_app/Features/home/peresntation/views/widgets/best_seller_list_view.dart';
 import 'package:book_app/Features/home/peresntation/views/widgets/custom_app_bar.dart';
 import 'package:book_app/Features/home/peresntation/views/widgets/featured_books_list_view.dart';
 import 'package:book_app/core/utils/styles.dart';
@@ -16,10 +16,7 @@ class HomeViewBoy extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CustomAppBar(),
-              Padding(
-                padding: EdgeInsets.only(left: 15, bottom: 9, right: 15),
-                child: FeaturedBooksListView(),
-              ),
+              FeaturedBooksListView(),
               SizedBox(
                 height: 10,
                 width: 15,
@@ -38,26 +35,6 @@ class HomeViewBoy extends StatelessWidget {
           child: BestSellerListView(),
         )
       ],
-    );
-  }
-}
-
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: BestSellerListViewItem(),
-        );
-      },
     );
   }
 }
