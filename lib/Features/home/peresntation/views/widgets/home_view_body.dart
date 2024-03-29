d56a5d2e3f2bfe9448ef683250cc28936e54076e
saraUsama9910 <sarausama80@gmail.com>
2024-03-29 11:01:19 +0200

    using custom scroll view

--- lib/Features/home/peresntation/views/widgets/home_view_body.dart
+++ lib/Features/home/peresntation/views/widgets/home_view_body.dart
@@ -9,6 +9,31 @@ class HomeViewBoy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(),
+            Padding(
+              padding: EdgeInsets.only(left: 15, bottom: 9, right: 15),
+              child: FeaturedBooksListView(),
+            ),
+            SizedBox(
+              height: 10,
+              width: 15,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15, left: 15, bottom: 15),
+              child: Text(
+                'Best Seller',
+                style: Styles.textStyle18,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -41,6 +66,7 @@ class BestSellerListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: ListView.builder(
+        padding: EdgeInsets.zero,
         itemCount: 10,
         itemBuilder: (context, index) {
           return const Padding(
