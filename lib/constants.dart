6cc54703f4c0a19891c35cd929d3058933ee862d
saraUsama9910 <sarausama80@gmail.com>
2024-04-17 20:29:05 +0200

    start after 15 day

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromARGB(255, 20, 20, 20);
 const kTransitionDuration = Duration(milliseconds: 250);
 const String kGtSectraFine = 'GT Sectra Fine';
+  
