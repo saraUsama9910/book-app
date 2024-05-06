66be32edbaddb679074ba8fdfffd760f732c722b
saraUsama9910 <sarausama80@gmail.com>
2024-05-06 18:50:24 +0300

    return homeRepo.fetchFeaturedBooks();

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,7 +1,13 @@
+import 'package:book_app/Features/home/domain/entities/book_entity.dart';
 import 'package:book_app/Features/home/domain/repos/home_repo.dart';
+import 'package:book_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 class FetchFeaturedBooksUseCase {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase({required this.homeRepo});
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    return homeRepo.fetchFeaturedBooks();
+  }
 }
