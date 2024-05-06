4ead2e7cac4e738c85a85d8963ce2ca7142c2b8b
saraUsama9910 <sarausama80@gmail.com>
2024-05-06 14:53:08 +0300

    EitherFailure,ListBookEntity

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -4,7 +4,7 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks();
-    Future<List<BookEntity>> fetchNewestBooks();
+    Future<Either<Failure,List<BookEntity>>> fetchNewestBooks();
 
 }
 
