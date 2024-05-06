a5f7d676246b3c5899ab33a02a6697190de39683
saraUsama9910 <sarausama80@gmail.com>
2024-05-06 14:42:05 +0300

    .

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -2,4 +2,7 @@ import 'package:book_app/Features/home/domain/entities/book_entity.dart';
 
 abstract class HomeRepo {
   Future<List<BookEntity>> fetchFeaturedBooks();
+    Future<List<BookEntity>> fetchNewestBooks();
+
 }
+
