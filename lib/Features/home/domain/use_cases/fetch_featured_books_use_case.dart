b370ea14745717052a08e6f6e3ca0f43376c2cf5
saraUsama9910 <sarausama80@gmail.com>
2024-05-06 19:23:00 +0300

    use case

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,30 @@
+import 'dart:ffi';
+
 import 'package:book_app/Features/home/domain/entities/book_entity.dart';
 import 'package:book_app/Features/home/domain/repos/home_repo.dart';
 import 'package:book_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>,NoParam>{
   final HomeRepo homeRepo;
 
-  FetchFeaturedBooksUseCase( this.homeRepo);
-  Future<Either<Failure, List<BookEntity>>> call() {
-    return homeRepo.fetchFeaturedBooks();
+  FetchFeaturedBooksUseCase({required this.homeRepo});
+  
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param])async {
+    throw await UnimplementedError();
   }
-}
+  
  
+  
+  
+  
+  
+ 
+ 
+}
+
+abstract class UseCase <Type,Param>{
+  Future<Either<Failure,Type>> call([Param param]);
+}
+class NoParam{}
