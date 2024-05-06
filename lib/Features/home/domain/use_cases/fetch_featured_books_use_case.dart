5781feccda51a04805f4cb27f6245fcdc93984e0
saraUsama9910 <sarausama80@gmail.com>
2024-05-06 18:48:08 +0300

    home repo

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,1 +1,7 @@
-class FetchFeaturedBooksUseCase{}
+import 'package:book_app/Features/home/domain/repos/home_repo.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase({required this.homeRepo});
+}
