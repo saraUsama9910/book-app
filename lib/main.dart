251cd4bea546e2c3bc7924d1e725a104d21ac469
saraUsama9910 <sarausama80@gmail.com>
2024-05-08 18:17:34 +0300

    initFlutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:book_app/constants.dart';
 import 'package:book_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main()async {
+void main() async {
   runApp(const BookApp());
+  Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
- await Hive.openBox(kFeaturedBox);
+  await Hive.openBox(kFeaturedBox);
 }
 
 class BookApp extends StatelessWidget {
