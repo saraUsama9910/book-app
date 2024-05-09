e6219560f799395529652c5138fd0d079ea3c929
saraUsama9910 <sarausama80@gmail.com>
2024-05-09 13:19:05 +0300

    create newest books cubit

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:book_app/Features/home/domain/entities/book_entity.dart';
+import 'package:book_app/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.newestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase newestBooksUseCase;
+  Future<void> featchFeaturedBooks() async {
+    emit(
+      NewestBooksLoading(),
+    );
+    var result = await newestBooksUseCase.call();
+    result.fold((failure) {
+      emit(
+        NewestBooksFailure(
+          failure.message,
+        ),
+      );
+    }, (books) {
+      emit(
+        NewestBooksSuccess(books),
+      );
+    });
+  }
+
+}
