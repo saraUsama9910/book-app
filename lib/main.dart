d1cab8f443b029a8e1e7062a2d78bbf9f3223ea3
saraUsama9910 <sarausama80@gmail.com>
2024-05-09 14:55:15 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hive_flutter/adapters.dart';
-
+ 
 void main() async {
   await Hive.initFlutter();
 
