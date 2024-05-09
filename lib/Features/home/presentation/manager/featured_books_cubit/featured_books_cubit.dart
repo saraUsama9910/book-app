5e4cd7c073456e200708c5891e325843c698b9f4
saraUsama9910 <sarausama80@gmail.com>
2024-05-09 13:08:08 +0300

    result

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:book_app/Features/home/domain/entities/book_entity.dart';
+import 'package:book_app/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  Future<void> featchFeaturedBooks() async {
+    emit(
+      FeaturedBooksLoading(),
+    );
+  var result=await  featuredBooksUseCase.call();
+  }
 }
